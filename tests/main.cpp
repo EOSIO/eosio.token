9c0e309815f7ef642f779a85034d39c032c87433
deck <deckb@@users.noreply.github.com>
2021-03-03 09:17:50 -0500

    cleanup cmake and tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -10,9 +10,7 @@
 #include <eosio/chain/exceptions.hpp>
 #include <Runtime/Runtime.h>
 
-#include "eosio.system_tester.hpp"
-
-using namespace eosio_system;
+// using namespace eosio_system;
 #define BOOST_TEST_STATIC_LINK
 
 void translate_fc_exception(const fc::exception &e) {
